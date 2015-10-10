066001bdf61ca921e43d7015d53aea7ea1b28bbe
Joey Nguyen <jnn193@gmail.com>
2015-10-10 15:47:24 -0500

    Added dynamic properties and nested components.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 // Define a 'class'
-var HelloWorld = React.createClass({
+var Badge = React.createClass({
     render: function() {
-        return <h1>Hello World</h1>;
+        return (
+            <div>
+                <button className="btn btn-primary" type="button">
+                    {this.props.title} <span className="badge">{this.props.number}</span>
+                </button>
+            </div>
+        );
+    }
+});
+var Form = React.createClass({
+    render: function() {
+        return (
+            <Badge title='Inbox' number='83' />
+        );
     }
 });
 
 // React, please render this class
-var element = React.createElement(HelloWorld);
+var form = React.createElement(Form);
 
 // React, after you render this class, place it in the '#container' div
-React.render(element, document.getElementById('container') );
+React.render(form, document.getElementById('container') );
