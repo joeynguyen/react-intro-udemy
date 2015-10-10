7e1736d9f14e6e4d0973646aaa446ba2d5eafa1f
Joey Nguyen <jnn193@gmail.com>
2015-10-10 17:27:13 -0500

    Created a thumbnail component.

--- js/main.js
+++ js/main.js
@@ -10,16 +10,33 @@ var Badge = React.createClass({
         );
     }
 });
-var Form = React.createClass({
+var Thumbnail = React.createClass({
     render: function() {
         return (
-            <Badge title='Inbox' number='83' />
+            <div className="thumbnail">
+                <img src={this.props.imageUrl} alt="..." />
+                <div className="caption">
+                    <h3>{this.props.header}</h3>
+                    <p>{this.props.description}</p>
+                    <p>
+                        <Badge title={this.props.title} number={this.props.number} />
+                    </p>
+                </div>
+            </div>
         );
     }
 });
 
+var options = {
+    title: 'See tutorials',
+    number: 13,
+    header: 'Learn React',
+    description: 'React is a new library for dynamic pages',
+    imageUrl: 'http://formatjs.io/img/react.svg'
+};
+
 // React, please render this class
-var form = React.createElement(Form);
+var thumbnail = React.createElement(Thumbnail, options);
 
 // React, after you render this class, place it in the '#container' div
-React.render(form, document.getElementById('container') );
+React.render(thumbnail, document.querySelector('.target') );
