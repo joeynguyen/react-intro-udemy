72351103e2bf815343e307c041157aa1271d1c89
Joey Nguyen <jnn193@gmail.com>
2015-10-10 23:50:28 -0500

    Updated CSS grid structure

--- js/main.js
+++ js/main.js
@@ -13,14 +13,16 @@ var Badge = React.createClass({
 var Thumbnail = React.createClass({
     render: function() {
         return (
-            <div className="thumbnail">
-                <img src={this.props.imageUrl} alt="..." />
-                <div className="caption">
-                    <h3>{this.props.header}</h3>
-                    <p>{this.props.description}</p>
-                    <p>
-                        <Badge title={this.props.title} number={this.props.number} />
-                    </p>
+            <div className="col-sm-6 col-md-4">
+                <div className="thumbnail">
+                    <img src={this.props.imageUrl} alt="..." />
+                    <div className="caption">
+                        <h3>{this.props.header}</h3>
+                        <p>{this.props.description}</p>
+                        <p>
+                            <Badge title={this.props.title} number={this.props.number} />
+                        </p>
+                    </div>
                 </div>
             </div>
         );
