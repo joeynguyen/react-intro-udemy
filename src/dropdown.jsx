55541592255b9b7911c30e5099521907f9b6554b
Joey Nguyen <jnn193@gmail.com>
2015-10-13 13:06:06 -0500

    Created and implemented list item component.

--- src/dropdown.jsx
+++ src/dropdown.jsx
@@ -1,13 +1,16 @@
 var React = require('react');
 var Button = require('./button');
-// var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
     handleClick: function() {
         alert('button clicked');
     },
     render: function() {
-        var visible;
+        var list = this.props.items.map(function(item) {
+            return <ListItem item={item} />
+        });
+        // var visible;
         // if(this.state.dropdownOpen) {
         //     visible = "visible";
         // } else {
@@ -16,7 +19,9 @@ module.exports = React.createClass({
         return (
             <div className="dropdown">
                 <Button handleClick={this.handleClick} className="btn-default" title={this.props.title} subTitleClassName="caret"/>
-                {/* <List /> */}
+                <ul>
+                    {list}
+                </ul>
             </div>
         );
     }
