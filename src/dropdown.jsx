70b6c92d5aa03eb90fb0b1dcf82d91996b24355d
Joey Nguyen <jnn193@gmail.com>
2015-10-14 00:21:35 -0500

    Added functionality to change dropdown button's text on selecting item and highlighting that item.

--- src/dropdown.jsx
+++ src/dropdown.jsx
@@ -4,19 +4,25 @@ var ListItem = require('./list-item');
 
 module.exports = React.createClass({
     getInitialState: function() {
-        return { open: false };
+        return { open: false, dropdownTitle: this.props.title };
     },
     handleClick: function() {
         this.state.open ? this.setState( {open: false} ) : this.setState( {open: true} );
         this.setState( {open: !this.state.open} );
     },
+    handleItemClick: function(itemName) {
+        this.setState({
+            open: false,
+            dropdownTitle: itemName
+        });
+    },
     render: function() {
         var list = this.props.items.map(function(item) {
-            return <ListItem key={item.id} item={item.name} />
-        });
+            return <ListItem className={this.state.dropdownTitle === item.name ? "active" : ""} key={item.id} itemName={item.name} whenItemClicked={this.handleItemClick} />
+        }.bind(this));
         return (
             <div className="dropdown">
-                <Button handleClick={this.handleClick} className="btn-default" title={this.props.title} subTitleClassName="caret"/>
+                <Button handleClick={this.handleClick} className="btn-default" title={this.state.dropdownTitle} subTitleClassName="caret"/>
                 <ul className={"dropdown-menu " + (this.state.open ? "show" : "")}>
                     {list}
                 </ul>
--- src/list-item.jsx
+++ src/list-item.jsx
@@ -1,7 +1,12 @@
 var React = require('react');
 
 module.exports = React.createClass({
+    handleClick: function() {
+        this.props.whenItemClicked(this.props.itemName);
+        // using this pattern allows you to add additional functionality to a click handler like:
+        // console.log('Additional functionality here');
+    },
     render: function() {
-        return <li><a>{this.props.item}</a></li>
+        return <li className={this.props.className}><a onClick={this.handleClick}>{this.props.itemName}</a></li>
     }
 });
