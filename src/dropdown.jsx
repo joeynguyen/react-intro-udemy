a8b011fee3b639eb591b6d87277cd2d056b60fe4
Joey Nguyen <jnn193@gmail.com>
2015-10-13 23:40:40 -0500

    Implemented component state and the ability to hide or show dropdown menu depending on its state.

--- src/dropdown.jsx
+++ src/dropdown.jsx
@@ -3,23 +3,21 @@ var Button = require('./button');
 var ListItem = require('./list-item');
 
 module.exports = React.createClass({
+    getInitialState: function() {
+        return { open: false };
+    },
     handleClick: function() {
-        alert('button clicked');
+        this.state.open ? this.setState( {open: false} ) : this.setState( {open: true} );
+        this.setState( {open: !this.state.open} );
     },
     render: function() {
         var list = this.props.items.map(function(item) {
             return <ListItem key={item.id} item={item.name} />
         });
-        // var visible;
-        // if(this.state.dropdownOpen) {
-        //     visible = "visible";
-        // } else {
-        //     visible = "hidden";
-        // }
         return (
             <div className="dropdown">
                 <Button handleClick={this.handleClick} className="btn-default" title={this.props.title} subTitleClassName="caret"/>
-                <ul>
+                <ul className={"dropdown-menu " + (this.state.open ? "show" : "")}>
                     {list}
                 </ul>
             </div>
